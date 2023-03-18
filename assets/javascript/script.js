30922b582b604b099e7359eafa803a460b983a80
aqeel garner <aqeel.garner1@gmail.com>
2023-03-18 10:18:25 +0000

    added event listeners

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,3 +12,40 @@ function time() {
     }, 1000);
 };
 time()
+var rightEls = document.querySelectorAll("#correct")
+var wrongEls = document.querySelectorAll("#wrong")
+var answerRightEls = document.querySelectorAll("#answerRight")
+var answerWrongEls = document.querySelectorAll("#answerWrong")
+var answerEl = "hidden"
+var answerWEl = "hidden"
+
+rightEls.forEach(function(rightEl) {
+    rightEl.addEventListener("click", function() {
+        if (answerEl === "hidden") {
+            answerEl = "view";
+            answerRightEls.forEach(function(answerRightEl) {
+              answerRightEl.style.display = "flex";
+        });
+        } else {
+            answerEl = "hidden";
+            answerRightEls.forEach(function(answerRightEl) {  
+              answerRightEl.style.display = "none";
+            });  
+        }
+    });
+});
+wrongEls.forEach(function(wrongEl) {
+    wrongEl.addEventListener("click", function() {
+        if (answerWEl === "hidden") {
+            answerWEl = "view";
+            answerWrongEls.forEach(function(answerWrongEl) {
+              answerWrongEl.style.display = "flex";
+        });
+        } else {
+            answerEl = "hidden";
+            answerWrongEls.forEach(function(answerWrongEl) {  
+              answerWrongEl.style.display = "none";
+            });  
+        }
+    });
+});
